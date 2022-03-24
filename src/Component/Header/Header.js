8b499dd85c94c6e5bcbd855935845a1ffe465871
Wooseok Jang <mmyu2090@gmail.com>
2022-03-25 02:21:23 +0900

    .wrap conflict modified

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
 
     render() {
         return (
-            <div className="wrap">
+            <div className="HeaderWrap">
                 <div className="menuBar">
                     <MenuBar />
                 </div>
