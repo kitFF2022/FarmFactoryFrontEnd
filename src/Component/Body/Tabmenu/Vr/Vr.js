86eb2166d5f5f994552f926dd055270ed942f3de
Miramu <ymdovmf00@gmail.com>
2022-03-26 19:12:54 +0900

    testt

--- src/Component/Body/Tabmenu/Vr/Vr.js
+++ src/Component/Body/Tabmenu/Vr/Vr.js
@@ -22,7 +22,7 @@ class Vr extends React.Component {
         <span className="Vrtext">
         <b>
         설비 모델링을 통한 3D로 간편하게 설계가 가능하며,<br/>
-        VR 기기가 있다면, MR을 통한 설계가 가능한 프로젝트.
+        VR 기기가 있다면, MR을 통한 설계가 가능한 프로젝트
         </b>
         </span>
         <br />
