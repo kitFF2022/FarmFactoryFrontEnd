c79b5c719fcf0fa8c1e95f59f30f68a8465e5af1
ldi2676 <jw2676@naver.com>
2022-03-28 17:14:51 +0900

    add Vrpaddingbox

--- src/Component/Body/Tabmenu/Vr/Vr.js
+++ src/Component/Body/Tabmenu/Vr/Vr.js
@@ -17,13 +17,13 @@ class Vr extends React.Component {
         <img className="Vrimg" src='/img/img1.jpg' alt="VR 사진">
         </img>
         <div className="Vrpaddingbox"><br/>
-        <span className='h1'>3D로<br/>입체감있게 </span><br/>
-        <span className="Vrtext">
-        <b>
-        설비 모델링을 통한 3D로 간편하게 설계가 가능하며,<br/>
-        VR 기기가 있다면, MR을 통한 설계가 가능한 프로젝트.
-        </b>
-        </span>
+          <span className='h1'>3D로<br/>입체감있게 </span><br/>
+          <span className="Vrtext">
+          <b>
+          설비 모델링을 통한 3D로 간편하게 설계가 가능하며,<br/>
+          VR 기기가 있다면, MR을 통한 설계가 가능한 프로젝트.
+          </b>
+          </span>
         <br /></div>
       </div>
 
