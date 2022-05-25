b13c6106d461979874da75c00320c33d3da1a48f
ldi2676 <jw2676@naver.com>
2022-05-25 22:59:56 +0900

    fix err

--- src/Component/Server/Menu/UserMenu.js
+++ src/Component/Server/Menu/UserMenu.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const api = 'http://mmyu.direct.quickconnect.to:8880'
 //유저 정보 수정
-function User_Data_Edit(){
+function User_Data_Edit() {
     window.location.href = "/UserEdit";
 }
 //전체 계정 삭제
@@ -13,49 +13,49 @@ function Sign_all_Delete() {
     const access_token = sessionStorage.getItem('user_token');
     var Email = sessionStorage.getItem('user_id');
     var PW = sessionStorage.getItem('user_pw');
-    if (window.confirm('정말로 현재 계정을 삭제하시겠습니까?') ===true){
+    if (window.confirm('정말로 현재 계정을 삭제하시겠습니까?') === true) {
 
         // console.log('계정이 삭제 되었습니다.');
-        axios.delete(api+"/user/userData", {
+        axios.delete(api + "/user/userData", {
             headers: {
                 Authorization: `Bearer ${access_token}`
             },
-            data:{
-                Emailaddr : Email,
+            data: {
+                Emailaddr: Email,
                 Password: PW
             }
         })
-        .then((response) => {
-            console.log(response.data.message)
-            alert('계정이 삭제 되었습니다.');
-            console.log('계정 삭제');
-            sessionStorage.removeItem('user_id');
-            sessionStorage.removeItem('user_token');
-            sessionStorage.removeItem('user_pw');
-            // window.location.href = "/";
-            
-        })
-        .catch((error) => {
-            console.error(error)
-            //Request failed with status code 422
-            if(error.message ==='Request failed with status code 422'){
-                alert('422 error : 문법오류');
+            .then((response) => {
+                console.log(response.data.message)
+                alert('계정이 삭제 되었습니다.');
+                console.log('계정 삭제');
+                sessionStorage.removeItem('user_id');
+                sessionStorage.removeItem('user_token');
+                sessionStorage.removeItem('user_pw');
+                window.location.href = "/";
 
-            }
-        });
+            })
+            .catch((error) => {
+                console.error(error)
+                //Request failed with status code 422
+                if (error.message === 'Request failed with status code 422') {
+                    alert('422 error : 문법오류');
+
+                }
+            });
     }
 }
 class Login extends React.Component {
     render() {
         return (
-            <div className = "Team_editPageWrap">
-                <Header/>
-                <div className = "Team_editWrap">
-                <img className="Team_editlogo"  src={process.env.PUBLIC_URL + '/img/logo.png'} alt='logo'></img>
-                <p><button className = "Loginbutton" type="button" 
-                onClick={User_Data_Edit}>정보 수정</button></p>
-                <p><button className = "Loginbutton" type="button" 
-                onClick={Sign_all_Delete}>계정 삭제</button></p>
+            <div className="Team_editPageWrap">
+                <Header />
+                <div className="Team_editWrap">
+                    <img className="Team_editlogo" src={process.env.PUBLIC_URL + '/img/logo.png'} alt='logo'></img>
+                    <p><button className="Loginbutton" type="button"
+                        onClick={User_Data_Edit}>정보 수정</button></p>
+                    <p><button className="Loginbutton" type="button"
+                        onClick={Sign_all_Delete}>계정 삭제</button></p>
                 </div>
             </div>
         );
