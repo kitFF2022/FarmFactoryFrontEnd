d7ad8c053fbabc07319589a13493e94406aa80a9
ldi2676 <jw2676@naver.com>
2022-06-02 21:12:33 +0900

    update 220602

--- src/Component/Server/Menu/UserMenu.js
+++ src/Component/Server/Menu/UserMenu.js
@@ -41,16 +41,16 @@ function Sign_all_Delete() {
                 if (error.message === 'Network Error') {
                     alert('422 No server response 서버가 동작하지 않습니다');
                 }
-                if (error.message === 'Request failed with status code 404') {
+                else if (error.message === 'Request failed with status code 404') {
                     alert('404 error 존재하지 않는 페이지 입니다');
                 }
-                if (error.message === 'Request failed with status code 409') {
+                else if (error.message === 'Request failed with status code 409') {
                     alert('409 error 데이터가 충돌했습니다. 팀을 삭제하셨는지 확인해 주세요.');
                 }
-                if (error.message === 'Request failed with status code 403') {
+                else if (error.message === 'Request failed with status code 403') {
                     alert('403 error 잘못된 데이터 입니다');
                 }
-                if (error.message === 'Request failed with status code 500') {
+                else if (error.message === 'Request failed with status code 500') {
                     alert('500 error 서버문제 입니다');
                 }
                 else {
